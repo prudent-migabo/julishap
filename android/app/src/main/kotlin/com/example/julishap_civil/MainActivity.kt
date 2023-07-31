6b6d253a19d9e73de483551b89b3756b2025f3b3
prudent16 <lyadungamigaboprudent@gmail.com>
2023-08-01 02:23:38 +0300

    feat: initial setup

---
+++ android/app/src/main/kotlin/com/example/julishap_civil/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.julishap_civil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'config/map_config.dart';
+import 'data/models/map_marker_model.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Julishap',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 33, 32, 32),
+        title: const Text('Flutter MapBox'),
+      ),
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              minZoom: 5,
+              maxZoom: 18,
+              zoom: 13,
+              center: MapConfig.myLocation,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                MapConfig.mapUrl,
+                additionalOptions: const {
+                  'mapStyleId': MapConfig.mapBoxStyleId,
+                  'accessToken': MapConfig.mapBoxAccessToken,
+                },
+              ),
+              MarkerLayer(
+                markers: [
+                  for (int i = 0; i < mapMarkers.length; i++)
+                    Marker(
+                      height: 40,
+                      width: 40,
+                      point: mapMarkers[i].location ?? MapConfig.myLocation,
+                      builder: (_) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: Image.asset(
+                            'assets/images/marker icon.png',
+                          ),
+                        );
+                      },
+                    ),
+                ],
+              ),
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
